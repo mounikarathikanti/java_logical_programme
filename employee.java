5d4e0a41b0371bf9b4d7026fa52a27a384c81e5f
mounikarathikanti <mounikamouni41590@gmail.com>
2024-11-07 21:45:45 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+class Employee
+{
+public static void main(Strings args[])
+{
+Scanner obj=new Scanner(system.in);
+int emp num;
+string emp name;
+float emp salary;
+system.out.println("enter employee number:");
+empnum=obj.nextInt()
+system.out.println("enter employee name:");
+empname=obj.nextstring()
+system.out.println("enter employee salary:");
+empsalary=obj.nextfloat();
+system.out.println("the given employee number is:"+empnum);
+system.out println("the given employee name is:"+empname);
+system.out.println("the given employee salary is:"+empsalary);
+}
